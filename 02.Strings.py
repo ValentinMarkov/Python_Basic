ff0be389b02655901695117b8db6aa00ccd10dd4
Valentin Markov <markov306@gmail.com>
2022-01-12 08:16:25 +0200

    update strings file with regions

--- 02.Strings.py
+++ 02.Strings.py
@@ -1,31 +1,38 @@
-# variables
+# region variables
 user_name = "Hans"
 user_age = "18"
 user_town = "Stara_Zagora"
+# endregion
 
-# string concatenations
+#  region string concatenations
 print("Hello I'm " + user_name)
 print(user_name + " is " + user_age + " years old")
 print(user_town + " is a biggest city in Bulgaria")
 print("Valentin\nMarkov")
+# endregion
 
-# work with strings
+# region work with strings
 phrase = "HedgeServe"
 print(phrase + " is my job")
 print(phrase.lower())
 print(phrase.islower())
 print(phrase.isupper())
 print(phrase.upper().isupper())
+# endregion
 
-# length of variable
+# region length of variable
 print(len(user_town))
+# endregion
 
-# get character number from string
+# region get character number from string
 print(user_town[6])
+# endregion
 
-# return index of specified character
+# region return index of specified character
 hero_name = "Ajax the Greek"
 print(hero_name.index("x"))
+# endregion
 
-# replace variable function
+# region replace variable function
 print(hero_name.replace("Ajax", "Timon"))
+# endregion
